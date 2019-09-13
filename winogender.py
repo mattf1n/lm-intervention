92f8ac7f02209733b8cccdddac2feca0ffb6b3f3
jessevig <jesse.vig@gmail.com>
2019-09-13 15:27:25 -0700

    Added Winogender analysis notebook. Refactored Winogender code.

--- winogender.py
+++ winogender.py
@@ -9,8 +9,8 @@ def load_examples(path='winogender_data/'):
         next(f, None)  # skip the headers
         for row in csv.reader(f, delimiter='\t'):
             occupation = row[0]
-            bergsma_pct_female[occupation] = row[1]
-            bls_pct_female[occupation] = row[2]
+            bergsma_pct_female[occupation] = float(row[1])
+            bls_pct_female[occupation] = float(row[2])
     examples = []
     with open(os.path.join(path, 'winogender_templates_filtered.tsv')) as f:
         next(f, None)  # skip the headers
@@ -18,22 +18,30 @@ def load_examples(path='winogender_data/'):
         for row in csv.reader(f, delimiter='\t'):
             row_pair.append(row)
             if len(row_pair) == 2:
-                base_string0, substitutes0, candidate0, occupation0, answer0 = _parse_row(row_pair[0])
-                base_string1, substitutes1, candidate1, occupation1, answer1 = _parse_row(row_pair[1])
-                assert base_string0 == base_string1
-                assert len(base_string0) > 0
-                assert '$' not in base_string0
-                assert substitutes0 == substitutes1
-                assert len(candidate0) > 0 and len(candidate1) > 0 and candidate0 != candidate1
-                if answer0 == 0:
-                    occupation_continuation = candidate0
-                    participant_continuation = candidate1
+                base_string1, substitute_female1, substitute_male1, candidate1, occupation1, answer1 = _parse_row(row_pair[0])
+                base_string2, substitute_female2, substitute_male2, candidate2, occupation2, answer2 = _parse_row(row_pair[1])
+                assert base_string1 == base_string2
+                assert len(base_string1) > 0
+                assert '$' not in base_string1
+                assert substitute_female1 == substitute_female2
+                assert substitute_male1 == substitute_male2
+                assert len(candidate1) > 0 and len(candidate2) > 0 and candidate1 != candidate2
+                if answer1 == 0:
+                    continuation_occupation = candidate1
+                    continuation_participant = candidate2
                 else:
-                    occupation_continuation = candidate1
-                    participant_continuation = candidate0
-                examples.append(WinogenderExample(base_string0, substitutes0, occupation_continuation,
-                                                  participant_continuation, occupation0,
-                                                  bergsma_pct_female[occupation0], bls_pct_female[occupation0]))
+                    continuation_occupation = candidate2
+                    continuation_participant = candidate1
+                examples.append(
+                    WinogenderExample(base_string1,
+                                      substitute_female1,
+                                      substitute_male1,
+                                      continuation_occupation,
+                                      continuation_participant,
+                                      occupation1,
+                                      bergsma_pct_female[occupation1],
+                                      bls_pct_female[occupation1])
+                )
                 row_pair = []
     return examples
 
@@ -50,18 +58,21 @@ def _parse_row(row):
             base_string = context.replace('$OCCUPATION', occupation)
             base_string = base_string.replace('$PARTICIPANT', participant)
             base_string = base_string + '{}'
-            return base_string, substitutes, candidate.strip(), occupation, answer
+            substitute_female = substitutes[0]
+            substitute_male = substitutes[1]
+            return base_string, substitute_female, substitute_male, candidate.strip(), occupation, int(answer)
     raise ValueError('Sentence does not contain pronoun type')
 
 
 class WinogenderExample():
 
-    def __init__(self, base_string, substitutes, occupation_continuation, participant_continuation, occupation,
+    def __init__(self, base_string, substitute_female, substitute_male, continuation_occupation, continuation_participant, occupation,
                  bergsma_pct_female, bls_pct_female):
         self.base_string = base_string
-        self.substitutes = substitutes
-        self.occupation_continuation = occupation_continuation
-        self.participant_continuation = participant_continuation
+        self.substitute_female = substitute_female
+        self.substitute_male = substitute_male
+        self.continuation_occupation = continuation_occupation
+        self.continuation_participant = continuation_participant
         self.occupation = occupation
         self.bergsma_pct_female = bergsma_pct_female
         self.bls_pct_female = bls_pct_female
@@ -69,9 +80,10 @@ class WinogenderExample():
     def __str__(self):
         return inspect.cleandoc(f"""
             base_string: {self.base_string}
-            substitutes: {self.substitutes}
-            occupation_continuation: {self.occupation_continuation}
-            participant_continuation: {self.participant_continuation}
+            substitute_female: {self.substitute_female}
+            substitute_male: {self.substitute_male}
+            continuation_occupation: {self.continuation_occupation}
+            continuation_participant: {self.continuation_participant}
             occupation: {self.occupation}
             bergsma_pct_female: {self.bergsma_pct_female}
             bls_pct_female: {self.bls_pct_female}
