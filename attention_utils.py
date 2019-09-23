29fbbad6240b69fa227a5231884e294d5f93007a
jessevig <jesse.vig@gmail.com>
2019-09-23 13:55:41 -0700

    Compute mean indirect and direct effects for winobias on both filtered and unfiltered versions.

--- attention_utils.py
+++ attention_utils.py
@@ -2,9 +2,12 @@ import torch
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 from tqdm import tqdm
+import pandas as pd
+import numpy as np
 
-def report_intervention(intervention, model, effects=('indirect', 'direct'), verbose=False):
 
+def perform_intervention(intervention, model, effects=('indirect', 'direct')):
+    """Perform intervention and return results for specified effects"""
     x = intervention.base_strings_tok[0]  # E.g. The doctor asked the nurse a question. He
     x_alt = intervention.base_strings_tok[1]  # E.g. The doctor asked the nurse a question. She
 
@@ -21,20 +24,47 @@ def report_intervention(intervention, model, effects=('indirect', 'direct'), ver
 
     odds_base = candidate2_base_prob / candidate1_base_prob
     odds_alt = candidate2_alt_prob / candidate1_alt_prob
-    odds_ratio_alt = odds_alt / odds_base
+    odds_ratio_total = odds_alt / odds_base
 
-    print(f"x : {intervention.base_strings[0]}")
-    print(f"x': {intervention.base_strings[1]}")
-    print(f"c1: {candidate1}")
-    print(f"c2: {candidate2}")
-    print(f"\np(c2|x) / p(c1|x) = {odds_base:.5f}")
-    print(f"p(c2|x') / p(c1|x') = {odds_alt:.5f}")
-    print(f"\nTOTAL Effect: (p(c2|x') / p(c1|x')) / (p(c2|x) / p(c1|x)) = {odds_ratio_alt:.3f}")
-
+    results = {
+        'base_string1': intervention.base_strings[0],
+        'base_string2': intervention.base_strings[1],
+        'candidate1': candidate1,
+        'candidate2': candidate2,
+        'candidate1_base_prob': candidate1_base_prob,
+        'candidate2_base_prob': candidate2_base_prob,
+        'odds_base': odds_base,
+        'candidate1_alt_prob': candidate1_alt_prob,
+        'candidate2_alt_prob': candidate2_alt_prob,
+        'odds_alt': odds_alt,
+        'odds_ratio_total': odds_ratio_total,
+    }
+    odds_ratio_indirect = None
+    odds_ratio_direct = None
     for effect in effects:
         candidate1_probs, candidate2_probs = model.attention_intervention_experiment(intervention, effect)
         odds_intervention = candidate2_probs / candidate1_probs
         odds_ratio_intervention = odds_intervention / odds_base
+        if effect == 'indirect':
+            odds_ratio_indirect = odds_ratio_intervention.tolist()
+        else:
+            odds_ratio_direct = odds_ratio_intervention.tolist()
+    results['odds_ratio_indirect'] = odds_ratio_indirect
+    results['odds_ratio_direct'] = odds_ratio_direct
+    return results
+
+
+def report_intervention(results, effects=('indirect', 'direct'), verbose=False):
+
+    print(f"x : {results['base_string1']}")
+    print(f"x': {results['base_string2']}")
+    print(f"c1: {results['candidate1']}")
+    print(f"c2: {results['candidate2']}")
+    print(f"\np(c2|x) / p(c1|x) = {results['odds_base']:.5f}")
+    print(f"p(c2|x') / p(c1|x') = {results['odds_alt']:.5f}")
+    print(f"\nTOTAL Effect: (p(c2|x') / p(c1|x')) / (p(c2|x) / p(c1|x)) = {results['odds_ratio_total']:.3f}")
+
+    for effect in effects:
         if verbose:
             print(f'\n{effect.upper()} Effect')
             if effect == 'indirect':
@@ -47,40 +77,31 @@ def report_intervention(intervention, model, effects=('indirect', 'direct'), ver
                 raise ValueError(f"Invalid effect: {effect}")
 
         plt.figure(figsize=(9, 7))
-        ax = sns.heatmap(odds_ratio_intervention, annot=True, annot_kws={"size": 12}, fmt=".2f")
+        ax = sns.heatmap(results['odds_ratio_' + effect], annot=True, annot_kws={"size": 12}, fmt=".2f")
         ax.set(xlabel='Head', ylabel='Layer', title=f'{effect.capitalize()} Effect')
 
 
-def report_interventions_summary(interventions, model, effects=('indirect', 'direct'), verbose=False):
-
-    odds_ratio_sum = {}
-    for effect in effects:
-        odds_ratio_sum[effect] = torch.zeros((model.num_layers, model.num_heads))
-    total_effect_sum = 0
+def perform_interventions(interventions, model, effects=('indirect', 'direct')):
+    results_list = []
     for intervention in tqdm(interventions):
-        x = intervention.base_strings_tok[0]
-        x_alt = intervention.base_strings_tok[1]
-        with torch.no_grad():
-            candidate1_base_prob, candidate2_base_prob = model.get_probabilities_for_examples(x,
-                                                                                              intervention.candidates_tok)
-            candidate1_alt_prob, candidate2_alt_prob = model.get_probabilities_for_examples(x_alt,
-                                                                                            intervention.candidates_tok)
-            odds_base = candidate2_base_prob / candidate1_base_prob
-            odds_alt = candidate2_alt_prob / candidate1_alt_prob
-            odds_ratio = odds_alt / odds_base
-            total_effect_sum += odds_ratio
-            for effect in effects:
-                candidate1_probs, candidate2_probs = model.attention_intervention_experiment(intervention, effect)
-                odds_intervention = candidate2_probs / candidate1_probs
-                odds_ratio_intervention = odds_intervention / odds_base
-                odds_ratio_sum[effect] += odds_ratio_intervention
+        results = perform_intervention(intervention, model, effects)
+        results_list.append(results)
+    return results_list
 
-    n_interventions = len(interventions)
+
+def report_interventions_summary(results, effects=('indirect', 'direct'), verbose=False):
+
+    df = pd.DataFrame(results)
+
     print('*** SUMMARY ***')
-    print(f"Num interventions: {n_interventions}")
-    print(f"Mean total effect: {total_effect_sum / n_interventions:.2f}")
+    print(f"Num interventions: {len(df)}")
+    print(f"Mean total effect: {df.odds_ratio_total.mean():.2f}")
 
     for effect in effects:
+        odds_ratio_intervention = np.stack(df['odds_ratio_' + effect].to_numpy()) # Convert column to 3d ndarray
+        mean_effect = odds_ratio_intervention.mean(axis=0)
+        # mean_effect = odds_ratio_intervention.mean() # This doesn't work when you load from csv file
+
         if verbose:
             print(f'\n{effect.upper()} Effect')
             if effect == 'indirect':
@@ -90,6 +111,36 @@ def report_interventions_summary(interventions, model, effects=('indirect', 'dir
                 print("   Intervention: replace x with x' while preserving Attn(x) in a specific layer/head")
                 print(f"   Effect = (p(c2|x', Attn(x)) / p(c1|x', Attn(x)) / (p(c2|x) / p(c1|x))")
         plt.figure(figsize=(9, 7))
-        ax = sns.heatmap(odds_ratio_sum[effect] / n_interventions, annot=True, annot_kws={"size": 12}, fmt=".2f")
+
+        ax = sns.heatmap(mean_effect, annot=True, annot_kws={"size": 12}, fmt=".2f")
         ax.set(xlabel='Head', ylabel='Layer', title=f'Mean {effect.capitalize()} Effect')
+
+if __name__ == "__main__":
+    from pytorch_transformers import GPT2Tokenizer
+    from experiment import Intervention, Model
+    from pandas import DataFrame
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    model = Model(output_attentions=True)
+
+    # Test experiment
+    interventions = [
+        Intervention(
+            tokenizer,
+            "The doctor asked the nurse a question. {}",
+            ["He", "She"],
+            ["asked", "answered"]),
+        Intervention(
+            tokenizer,
+            "The doctor asked the nurse a question. {}",
+            ["He", "She"],
+            ["requested", "responded"])
+    ]
+
+    results = perform_interventions(interventions, model)
+    report_interventions_summary(results)
+
+    df = DataFrame(results)
+    report_interventions_summary(df)
+
+
 
