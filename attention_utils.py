71d82f09d49d6bc9ee6a2aec775641ea7b62c0b4
jessevig <jesse.vig@gmail.com>
2019-09-28 05:55:40 -0700

    Use proportional difference (odds ratio - 1) instead of odds ratio. Perform attention intervention across entire layer.

--- attention_utils.py
+++ attention_utils.py
@@ -7,7 +7,7 @@ import numpy as np
 from scipy.stats import ttest_ind
 
 
-def perform_intervention(intervention, model, effects=('indirect', 'direct')):
+def perform_intervention(intervention, model, effect_types=('indirect', 'direct')):
     """Perform intervention and return results for specified effects"""
     x = intervention.base_strings_tok[0]  # E.g. The doctor asked the nurse a question. He
     x_alt = intervention.base_strings_tok[1]  # E.g. The doctor asked the nurse a question. She
@@ -25,7 +25,7 @@ def perform_intervention(intervention, model, effects=('indirect', 'direct')):
 
     odds_base = candidate2_base_prob / candidate1_base_prob
     odds_alt = candidate2_alt_prob / candidate1_alt_prob
-    odds_ratio_total = odds_alt / odds_base
+    total_effect = (odds_alt - odds_base) / odds_base
 
     results = {
         'base_string1': intervention.base_strings[0],
@@ -38,24 +38,22 @@ def perform_intervention(intervention, model, effects=('indirect', 'direct')):
         'candidate1_alt_prob': candidate1_alt_prob,
         'candidate2_alt_prob': candidate2_alt_prob,
         'odds_alt': odds_alt,
-        'odds_ratio_total': odds_ratio_total,
+        'total_effect': total_effect,
     }
-    odds_ratio_indirect = None
-    odds_ratio_direct = None
-    for effect in effects:
-        candidate1_probs, candidate2_probs = model.attention_intervention_experiment(intervention, effect)
-        odds_intervention = candidate2_probs / candidate1_probs
-        odds_ratio_intervention = odds_intervention / odds_base
-        if effect == 'indirect':
-            odds_ratio_indirect = odds_ratio_intervention.tolist()
-        else:
-            odds_ratio_direct = odds_ratio_intervention.tolist()
-    results['odds_ratio_indirect'] = odds_ratio_indirect
-    results['odds_ratio_direct'] = odds_ratio_direct
+
+    for effect_type in effect_types:
+        candidate1_probs_head, candidate2_probs_head, candidate1_probs_layer, candidate2_probs_layer = model.attention_intervention_experiment(intervention, effect_type)
+        odds_intervention_head = candidate2_probs_head / candidate1_probs_head
+        odds_intervention_layer = candidate2_probs_layer / candidate1_probs_layer
+        effect_head = (odds_intervention_head - odds_base) / odds_base
+        effect_layer = (odds_intervention_layer - odds_base) / odds_base
+        results[effect_type + "_effect_head"] = effect_head.tolist()
+        results[effect_type + "_effect_layer"] = effect_layer.tolist()
+
     return results
 
 
-def report_intervention(results, effects=('indirect', 'direct'), verbose=False):
+def report_intervention(results, effect_types=('indirect', 'direct'), verbose=False):
     """Report results for single intervention"""
 
     print(f"x : {results['base_string1']}")
@@ -64,76 +62,92 @@ def report_intervention(results, effects=('indirect', 'direct'), verbose=False):
     print(f"c2: {results['candidate2']}")
     print(f"\np(c2|x) / p(c1|x) = {results['odds_base']:.5f}")
     print(f"p(c2|x') / p(c1|x') = {results['odds_alt']:.5f}")
-    print(f"\nTOTAL Effect: (p(c2|x') / p(c1|x')) / (p(c2|x) / p(c1|x)) = {results['odds_ratio_total']:.3f}")
+    print(f"\nTOTAL Effect: (p(c2|x') / p(c1|x')) / (p(c2|x) / p(c1|x)) - 1 = {results['total_effect']:.3f}")
 
-    for effect in effects:
+    for effect_type in effect_types:
         if verbose:
-            print(f'\n{effect.upper()} Effect')
-            if effect == 'indirect':
+            print(f'\n{effect_type.upper()} Effect')
+            if effect_type == 'indirect':
                 print("   Intervention: replace Attn(x) with Attn(x') in a specific layer/head")
-                print(f"   Effect = (p(c2|x, Attn(x')) / p(c1|x, Attn(x')) / (p(c2|x) / p(c1|x))")
-            elif effect == 'direct':
+                print(f"   Effect = (p(c2|x, Attn(x')) / p(c1|x, Attn(x')) / (p(c2|x) / p(c1|x)) - 1")
+            elif effect_type == 'direct':
                 print("   Intervention: replace x with x' while preserving Attn(x) in a specific layer/head")
-                print(f"   Effect = (p(c2|x', Attn(x)) / p(c1|x', Attn(x)) / (p(c2|x) / p(c1|x))")
-            else:
-                raise ValueError(f"Invalid effect: {effect}")
+                print(f"   Effect = (p(c2|x', Attn(x)) / p(c1|x', Attn(x)) / (p(c2|x) / p(c1|x)) - 1")
 
         plt.figure(figsize=(9, 7))
-        ax = sns.heatmap(results['odds_ratio_' + effect], annot=True, annot_kws={"size": 12}, fmt=".2f")
-        ax.set(xlabel='Head', ylabel='Layer', title=f'{effect.capitalize()} Effect')
+        ax = sns.heatmap(results[effect_type + '_effect_head'], annot=True, annot_kws={"size": 12}, fmt=".2f")
+        ax.set(xlabel='Head', ylabel='Layer', title=f'{effect_type.capitalize()} Effect')
 
 
-def perform_interventions(interventions, model, effects=('indirect', 'direct')):
+def perform_interventions(interventions, model, effect_types=('indirect', 'direct')):
     """Perform multiple interventions"""
     results_list = []
     for intervention in tqdm(interventions):
-        results = perform_intervention(intervention, model, effects)
+        results = perform_intervention(intervention, model, effect_types)
         results_list.append(results)
     return results_list
 
 
-def report_interventions_summary(results, effects=('indirect', 'direct'), verbose=False, k=10):
-    """Report summary results for multiple interventions"""
+def report_interventions_summary_by_head(results, effect_types=('indirect', 'direct'), verbose=False, k=10):
+    """Report summary results for multiple interventions by head"""
 
     df = pd.DataFrame(results)
 
-    print('*** SUMMARY ***')
+    print('*** SUMMARY BY HEAD ***')
     print(f"Num interventions: {len(df)}")
-    print(f"Mean total effect: {df.odds_ratio_total.mean():.2f}")
+    print(f"Mean total effect: {df.total_effect.mean():.2f}")
 
-    for effect in effects:
-        odds_ratio_intervention = np.stack(df['odds_ratio_' + effect].to_numpy()) # Convert column to 3d ndarray (num_examples x num_layers x num_heads)
-        mean_effect = odds_ratio_intervention.mean(axis=0)
-        if effect == 'indirect':
+    for effect_type in effect_types:
+        effect = np.stack(df[effect_type + '_effect_head'].to_numpy())  # Convert column to 3d ndarray (num_examples x num_layers x num_heads)
+        mean_effect = effect.mean(axis=0)
+        if effect_type == 'indirect':
             ranking_metric = mean_effect
         else:
             ranking_metric = -mean_effect
         topk_indices = _topk_indices(ranking_metric, k)
 
         # Compute significance levels
-        all_values = odds_ratio_intervention.flatten()
-        print(f'\n{effect.upper()} Effect (mean = {all_values.mean()})')
+        all_values = effect.flatten()
+        print(f'\n{effect_type.upper()} Effect (mean = {all_values.mean()})')
         print(f"Top {k} heads:")
         for ind in topk_indices:
             layer, head = np.unravel_index(ind, mean_effect.shape)
-            head_values = odds_ratio_intervention[:, layer, head].flatten()
+            head_values = effect[:, layer, head].flatten()
             tstatistic, pvalue = ttest_ind(head_values, all_values)
-            if effect == 'indirect':
+            if effect_type == 'indirect':
                 assert tstatistic > 0
             else:
                 assert tstatistic < 0
             one_tailed_pvalue = pvalue / 2
             print(f'   {layer} {head}: {mean_effect[layer, head]} (p={one_tailed_pvalue:.4f})')
         if verbose:
-            if effect == 'indirect':
+            if effect_type == 'indirect':
                 print("   Intervention: replace Attn(x) with Attn(x') in a specific layer/head")
-                print(f"   Effect = (p(c2|x, Attn(x')) / p(c1|x, Attn(x')) / (p(c2|x) / p(c1|x))")
-            elif effect == 'direct':
+                print(f"   Effect = (p(c2|x, Attn(x')) / p(c1|x, Attn(x')) / (p(c2|x) / p(c1|x)) - 1")
+            elif effect_type == 'direct':
                 print("   Intervention: replace x with x' while preserving Attn(x) in a specific layer/head")
-                print(f"   Effect = (p(c2|x', Attn(x)) / p(c1|x', Attn(x)) / (p(c2|x) / p(c1|x))")
+                print(f"   Effect = (p(c2|x', Attn(x)) / p(c1|x', Attn(x)) / (p(c2|x) / p(c1|x)) - 1")
         plt.figure(figsize=(9, 7))
         ax = sns.heatmap(mean_effect, annot=True, annot_kws={"size": 12}, fmt=".2f")
-        ax.set(xlabel='Head', ylabel='Layer', title=f'Mean {effect.capitalize()} Effect')
+        ax.set(xlabel='Head', ylabel='Layer', title=f'Mean {effect_type.capitalize()} Effect')
+
+def report_interventions_summary_by_layer(results, effect_types=('indirect', 'direct')):
+    """Report summary results for multiple interventions by layer"""
+
+    df = pd.DataFrame(results)
+
+    print('*** SUMMARY BY LAYER ***')
+    print(f"Num interventions: {len(df)}")
+    print(f"Mean total effect: {df.total_effect.mean():.2f}")
+
+    for effect_type in effect_types:
+        effect = np.stack(df[effect_type + '_effect_layer'].to_numpy())  # Convert column to 2d ndarray (num_examples x num_layers)
+        mean_effect = effect.mean(axis=0)
+        n_layers = mean_effect.shape[0]
+
+        plt.figure(figsize=(9, 7))
+        ax = sns.barplot(x=mean_effect, y=list(range(n_layers)), color="blue", saturation=.3, orient="h")
+        ax.set(ylabel='Layer', title=f'Mean {effect_type.capitalize()} Effect')
 
 
 def _topk_indices(arr, k):
@@ -163,7 +177,7 @@ if __name__ == "__main__":
     ]
 
     results = perform_interventions(interventions, model)
-    report_interventions_summary(results)
+    report_interventions_summary_by_layer(results)
 
 
 
