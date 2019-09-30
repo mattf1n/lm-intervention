bfcaf3f6fcfad1e067fe9b4917b9f6ed528d1438
Sebastian Gehrmann <s.gehrmann@outlook.com>
2019-09-30 09:38:05 -0400

    fix context encoding

--- experiment.py
+++ experiment.py
@@ -1,6 +1,6 @@
 
 import torch
-import torch.nn as nn
+# import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 # import random
@@ -10,13 +10,13 @@ from tqdm import tqdm
 import math
 import statistics
 
-from collections import Counter, defaultdict
+# from collections import Counter, defaultdict
 
 # import pandas as pd
 # import seaborn as sns
 # import matplotlib.pyplot as plt
 
-from pytorch_transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from gpt2_attention import AttentionOverride
 
 # sns.set(style="ticks", color_codes=True)
@@ -45,24 +45,33 @@ class Intervention():
         # Tokenized bases
         self.base_strings_tok = [self.enc.encode(s)
                                  for s in self.base_strings]
+        self.base_strings_tok = torch.LongTensor(self.base_strings_tok)
+        self.base_strings_tok.to(device)
         # Where to intervene
         self.position = base_string.split().index('{}')
 
         self.candidates = []
         for c in candidates:
-            tokens = self.enc.tokenize('. ' + c)[1:] # '. ' added to input so that tokenizer understand that first word follows a space.
+            # '. ' added to input so that tokenizer understand that first word follows a space.
+            tokens = self.enc.tokenize('. ' + c)[1:]
             self.candidates.append(tokens)
 
-        self.candidates_tok = [self.enc.convert_tokens_to_ids(tokens) for tokens in self.candidates]
+        self.candidates_tok = [self.enc.convert_tokens_to_ids(tokens)
+                               for tokens in self.candidates]
 
 
 class Model():
     '''
     Wrapper for all model logic
     '''
-    def __init__(self, device='cpu', output_attentions=False, gpt2_version='gpt2'):
+    def __init__(self,
+                 device='cpu',
+                 output_attentions=False,
+                 gpt2_version='gpt2'):
         super()
-        self.model = GPT2LMHeadModel.from_pretrained(gpt2_version, output_attentions=output_attentions)
+        self.model = GPT2LMHeadModel.from_pretrained(
+            gpt2_version,
+            output_attentions=output_attentions)
         self.model.eval()
         self.model.to(device)
 
@@ -77,7 +86,12 @@ class Model():
 
     def get_representations(self, context, position):
         # Hook for saving the representation
-        def extract_representation_hook(module, input, output, position, representations, layer):
+        def extract_representation_hook(module,
+                                        input,
+                                        output,
+                                        position,
+                                        representations,
+                                        layer):
             representations[layer] = output[0][position]
         handles = []
         representation = {}
@@ -98,13 +112,15 @@ class Model():
 
     def get_probabilities_for_examples(self, context, candidates):
         """
-        Return probability of candidates given context. Prob of each candidate is normalized by length of candidate.
+        Return probability of candidates given context.
+        Prob of each candidate is normalized by length of candidate.
 
         Args:
             context: list of token ids in context
             candidates: list of list of token ids in each candidate
 
-        Returns: list containing probability for each candidate (normalized by length of candidate)
+        Returns: list containing probability for each candidate
+                      (normalized by length of candidate)
         """
 
         max_len = max(len(c) for c in candidates)
@@ -120,14 +136,18 @@ class Model():
             mean_probs = []
             for candidate in candidates:
                 combined = context + candidate
-                batch = torch.tensor(combined[:-1]).unsqueeze(dim=0) # Exclude last token position when predicting next token
-                logits = self.model(batch)[0] # Shape (batch_size, seq_len, vocab_size)
-                log_probs = F.log_softmax(logits[-1, :, :], dim=-1) # Shape (seq_len, vocab_size)
+                # Exclude last token position when predicting next token
+                batch = torch.tensor(combined[:-1]).unsqueeze(dim=0)
+                # Shape (batch_size, seq_len, vocab_size)
+                logits = self.model(batch)[0]
+                # Shape (seq_len, vocab_size)
+                log_probs = F.log_softmax(logits[-1, :, :], dim=-1)
                 context_end_pos = len(context) - 1
                 continuation_end_pos = context_end_pos + len(candidate)
                 token_log_probs = []
                 # TODO: Vectorize this
-                for i in range(context_end_pos, continuation_end_pos): # Up to but not including last token position
+                # Up to but not including last token position
+                for i in range(context_end_pos, continuation_end_pos):
                     next_token_id = combined[i+1]
                     next_token_log_prob = log_probs[i][next_token_id].item()
                     token_log_probs.append(next_token_log_prob)
@@ -146,13 +166,19 @@ class Model():
                             intervention_type='diff',
                             alpha=1.):
         # Hook for changing representation during forward pass
-        def intervention_hook(module, input, output, position, neuron, intervention, intervention_type):
+        def intervention_hook(module,
+                              input,
+                              output,
+                              position,
+                              neuron,
+                              intervention,
+                              intervention_type):
             if intervention_type == 'replace':
                 output[0, position, neuron] = intervention
             elif intervention_type == 'diff':
                 output[0, position, neuron] += intervention
             else:
-                raise ValueError(f"Invalid intervention_type: {intervention_type}")                
+                raise ValueError(f"Invalid intervention_type: {intervention_type}")
 
         intervention_rep = alpha * rep[layer][neuron]
         mlp_intervention_handle = self.model.transformer.h[layer]\
@@ -203,34 +229,45 @@ class Model():
             context: context text
             outputs: candidate outputs
             layer: layer in which to intervene
-            attn_override: values to override the computed attention weights. Shape is [batch_size, num_heads, seq_len, seq_len]
-            attn_override_mask: indicates which attention weights to override. Shape is [batch_size, num_heads, seq_len, seq_len]
+            attn_override: values to override the computed attention weights.
+                           Shape is [batch_size, num_heads, seq_len, seq_len]
+            attn_override_mask: indicates which attention weights to override.
+                                Shape is [batch_size, num_heads, seq_len, seq_len]
         """
 
         def intervention_hook(module, input, outputs):
-            attention_override_module = AttentionOverride(module, attn_override, attn_override_mask)
+            attention_override_module = AttentionOverride(
+                module, attn_override, attn_override_mask)
             outputs[:] = attention_override_module(*input)
 
         with torch.no_grad():
-            hook = self.model.transformer.h[layer].attn.register_forward_hook(intervention_hook)
+            hook = self.model.transformer.h[layer].attn.register_forward_hook(
+                intervention_hook)
             new_probabilities = self.get_probabilities_for_examples(
                 context,
                 outputs)
             hook.remove()
             return new_probabilities
 
-    def neuron_intervention_experiment(self, word2intervention, intervention_type, alpha):
+    def neuron_intervention_experiment(self,
+                                       word2intervention,
+                                       intervention_type,
+                                       alpha=1):
         """
         run multiple intervention experiments
         """
 
         word2intervention_results = {}
         for word in tqdm(word2intervention, desc='words'):
-            word2intervention_results[word] = self.neuron_intervention_single_experiment(word2intervention[word], intervention_type, alpha)
+            word2intervention_results[word] = self.neuron_intervention_single_experiment(
+                word2intervention[word], intervention_type, alpha)
 
         return word2intervention_results
 
-    def neuron_intervention_single_experiment(self, intervention, intervention_type, alpha=1):
+    def neuron_intervention_single_experiment(self,
+                                              intervention,
+                                              intervention_type,
+                                              alpha=1):
         """
         run one full neuron intervention experiment
         """
@@ -249,35 +286,43 @@ class Model():
                 intervention.base_strings_tok[2],
                 intervention.position)
 
-            # TODO: this whole logic can probably be improved 
+            # TODO: this whole logic can probably be improved
             # determine effect type and set representations
+
+            # e.g. The teacher said that
             if intervention_type == 'man_minus_woman':
-                context = intervention.base_strings_tok[0] # e.g. The teacher said that
-                rep = {k: v - woman_representations[k] for k, v in man_representations.items()}
+                context = intervention.base_strings_tok[0]
+                rep = {k: v - woman_representations[k]
+                       for k, v in man_representations.items()}
                 replace_or_diff = 'diff'
+            # e.g. The teacher said that
             elif intervention_type == 'woman_minus_man':
-                context = intervention.base_strings_tok[0] # e.g. The teacher said that
-                rep = {k: v - man_representations[k] for k, v in woman_representations.items()}
+                context = intervention.base_strings_tok[0]
+                rep = {k: v - man_representations[k]
+                       for k, v in woman_representations.items()}
                 replace_or_diff = 'diff'
+            # e.g. The man said that
             elif intervention_type == 'man_direct':
-                context = intervention.base_strings_tok[1] # e.g. The man said that 
+                context = intervention.base_strings_tok[1]
                 rep = base_representations
                 replace_or_diff = 'replace'
+            # e.g. The teacher said that
             elif intervention_type == 'man_indirect':
-                context = intervention.base_strings_tok[0] # e.g. The teacher said that
+                context = intervention.base_strings_tok[0]
                 rep = man_representations
                 replace_or_diff = 'replace'
+            # e.g. The woman said that
             elif intervention_type == 'woman_direct':
-                context = intervention.base_strings_tok[2] # e.g. The woman said that 
+                context = intervention.base_strings_tok[2]
                 rep = base_representations
                 replace_or_diff = 'replace'
+            # e.g. The teacher said that
             elif intervention_type == 'woman_indirect':
-                context = intervention.base_strings_tok[0] # e.g. The teacher said that
+                context = intervention.base_strings_tok[0]
                 rep = woman_representations
                 replace_or_diff = 'replace'
             else:
-                raise ValueError(f"Invalid intervention_type: {intervention_type}")  
-            
+                raise ValueError(f"Invalid intervention_type: {intervention_type}")
 
             # Probabilities without intervention (Base case)
             candidate1_base_prob, candidate2_base_prob = self.get_probabilities_for_examples(
@@ -307,17 +352,20 @@ class Model():
                         intervention_type=replace_or_diff,
                         alpha=alpha)
 
-        return (candidate1_base_prob, candidate2_base_prob, 
-                candidate1_alt1_prob, candidate2_alt1_prob, 
-                candidate1_alt2_prob, candidate2_alt2_prob, 
+        return (candidate1_base_prob, candidate2_base_prob,
+                candidate1_alt1_prob, candidate2_alt1_prob,
+                candidate1_alt2_prob, candidate2_alt2_prob,
                 candidate1_probs, candidate2_probs)
 
     def attention_intervention_experiment(self, intervention, effect):
         """
-        Run one full attention intervention experiment measuring indirect or direct effect.
+        Run one full attention intervention experiment
+        measuring indirect or direct effect.
         """
-        x = intervention.base_strings_tok[0] # E.g. The doctor asked the nurse a question. He
-        x_alt = intervention.base_strings_tok[1] # E.g. The doctor asked the nurse a question. She
+        # E.g. The doctor asked the nurse a question. He
+        x = intervention.base_strings_tok[0]
+        # E.g. The doctor asked the nurse a question. She
+        x_alt = intervention.base_strings_tok[1]
 
         if effect == 'indirect':
             input = x_alt  # Get attention for x_alt
@@ -368,10 +416,9 @@ class Model():
 
         return candidate1_probs_head, candidate2_probs_head, candidate1_probs_layer, candidate2_probs_layer
 
-
-
     def _tok_to_batch(self, tok_ids):
         return torch.tensor(tok_ids).unsqueeze(0)
+
 
 def main():
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
@@ -385,10 +432,13 @@ def main():
             [biased_word, "man", "woman"],
             ["he", "she"])
 
-    c1, c1_probs, c2, c2_probs = model.neuron_intervention_experiment(intervention)
+    c1, c1_probs, c2, c2_probs = model.neuron_intervention_experiment(
+        {biased_word: intervention},
+        'man_minus_woman')
     print('more probable candidate per layer, across all neurons in the layer')
     print('candidate1:', intervention.candidates[0], c1)
     print('candidate2:', intervention.candidates[1], c2)
+
 
 if __name__ == "__main__":
     main()
