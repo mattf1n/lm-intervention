8e94dc45e2634273793b8e02798f5dd69b9889bb
jessevig <jesse.vig@gmail.com>
2019-10-25 16:59:20 -0700

    - Minor updates to attention intervention and reporting - Added results for Winobias test dataset - Added summary report to results

--- winobias_attn_intervention.py
+++ winobias_attn_intervention.py
@@ -1,10 +1,11 @@
+from attention_utils import perform_interventions, get_odds_ratio
 import fire
 import winobias
 from experiment import Model
-from attention_utils import perform_interventions
 from transformers import GPT2Tokenizer
 import json
-import pandas as pd
+from pandas import DataFrame
+
 
 def intervene_attention(gpt2_version, do_filter, split, device='cuda', filter_quantile=0.25):
     if split == 'dev':
@@ -20,7 +21,8 @@ def intervene_attention(gpt2_version, do_filter, split, device='cuda', filter_qu
             'split': split,
             'num_examples_loaded': len(examples)}
     if do_filter:
-        df = winobias.analyze(examples, gpt2_version=gpt2_version)
+        interventions = [ex.to_intervention(tokenizer) for ex in examples]
+        df = DataFrame({'odds_ratio': [get_odds_ratio(intervention, model) for intervention in interventions]})
         df_expected = df[df.odds_ratio > 1]
         threshold = df_expected.odds_ratio.quantile(filter_quantile)
         filtered_examples = []
@@ -30,8 +32,10 @@ def intervene_attention(gpt2_version, do_filter, split, device='cuda', filter_qu
             odds_ratio = df.iloc[i].odds_ratio
             if odds_ratio > threshold:
                 filtered_examples.append(ex)
+
         print(f'Num examples with odds ratio > 1: {len(df_expected)} / {len(examples)}')
-        print(f'Num examples with odds ratio > {threshold:.4f} ({filter_quantile} quantile): {len(filtered_examples)} / {len(examples)}')
+        print(
+            f'Num examples with odds ratio > {threshold:.4f} ({filter_quantile} quantile): {len(filtered_examples)} / {len(examples)}')
         json_data['num_examples_aligned'] = len(df_expected)
         json_data['filter_quantile'] = filter_quantile
         json_data['threshold'] = threshold
@@ -39,7 +43,7 @@ def intervene_attention(gpt2_version, do_filter, split, device='cuda', filter_qu
     json_data['num_examples_analyzed'] = len(examples)
     interventions = [ex.to_intervention(tokenizer) for ex in examples]
     results = perform_interventions(interventions, model)
-    json_data['mean_total_effect'] = pd.DataFrame(results).total_effect.mean()
+    json_data['mean_total_effect'] = DataFrame(results).total_effect.mean()
     filter_name = 'filtered' if do_filter else 'unfiltered'
     fname = f"winobias_data/attention_intervention_{gpt2_version}_{filter_name}_{split}.json"
     json_data['results'] = results
