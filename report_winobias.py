fda8e0455b2f6cc043767f0a7079813741a4f668
jessevig <jesse.vig@gmail.com>
2019-10-29 16:25:54 -0700

    Compute mean sum indirect effects.

--- report_winobias.py
+++ report_winobias.py
@@ -1,9 +1,10 @@
 import json
 from prettytable import PrettyTable
-
+import pandas as pd
+import numpy as np
 
 def main():
-    fields = ['model_version', 'do_filter', 'split', 'mean_total_effect', 'prop_aligned', 'num_examples_loaded',
+    fields = ['model_version', 'do_filter', 'split', 'mean_total_effect', 'mean_sum_indirect_effect', 'prop_aligned', 'num_examples_loaded',
               'num_examples_analyzed', 'filter_quantile', 'threshold', 'num_examples_aligned']
     t = PrettyTable(fields)
     for model_version in ['distilgpt2', 'gpt2', 'gpt2-medium', 'gpt2-large']:
@@ -12,10 +13,17 @@ def main():
                 fname =  f"winobias_data/attention_intervention_{model_version}_{filter}_{split}.json"
                 with open(fname) as f:
                     data = json.load(f)
+                    # Populate 'prop_aligned'
                     try:
                         data['prop_aligned'] = data['num_examples_aligned'] / data['num_examples_loaded']
                     except KeyError:
                         data['prop_aligned'] = ''
+                    # Populate mean_sum_indirect
+                    results = data['results']
+                    df = pd.DataFrame(results)
+                    # Convert to shape (num_examples X num_layers X num_heads)
+                    indirect_by_head = np.stack(df['indirect_effect_head'].to_numpy())
+                    data['mean_sum_indirect_effect'] = indirect_by_head.sum(axis=(1, 2)).mean()
                     try:
                         t.add_row([data.get(field, '') for field in fields])
                     except (KeyError, AttributeError):
