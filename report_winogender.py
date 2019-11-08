f609a7dc784198e220c5dcd8493c09e41b01e84c
jessevig <jesse.vig@gmail.com>
2019-11-07 18:48:40 -0800

    Generate latex table for total effects

--- report_winogender.py
+++ report_winogender.py
@@ -3,17 +3,37 @@ from prettytable import PrettyTable
 import pandas as pd
 import numpy as np
 
+model_to_size = {
+    'distilgpt2': '-37\\%',
+    'gpt2': '124M',
+    'gpt2-medium': '355M',
+    'gpt2-large': '774M'
+}
+model_to_name = {
+    'distilgpt2': 'Distill-GPT2',
+    'gpt2': 'GPT2-small',
+    'gpt2-medium': 'GPT2-medium',
+    'gpt2-large': 'GPT2-large'
+}
+
 def main():
     fields = ['model_version', 'do_filter', 'split', 'mean_total_effect', 'mean_model_indirect_effect',
               'mean_model_direct_effect', 'mean_sum_indirect_effect', 'prop_aligned', 'num_examples_loaded',
               'num_examples_analyzed']
     t = PrettyTable(fields)
-    for model_version in ['distilgpt2', 'gpt2', 'gpt2-medium', 'gpt2-large']:
-        for filter in ['filtered', 'unfiltered']:
+    for filter in ['filtered', 'unfiltered']:
+        latex =  '\\hline\nModel & Param & TE (BLS) & TE (Bergsma) \\\\\n\\hline\n'
+        for model_version in ['distilgpt2', 'gpt2', 'gpt2-medium', 'gpt2-large']:
+            bergsma_effect = None
+            bls_effect = None
             for stat in ['bergsma', 'bls']:
                 fname =  f"winogender_data/attention_intervention_{stat}_{model_version}_{filter}.json"
                 with open(fname) as f:
                     data = json.load(f)
+                    if stat == 'bergsma':
+                        bergsma_effect = data['mean_total_effect']
+                    else:
+                        bls_effect =  data['mean_total_effect']
                     data['stat'] = stat
                     try:
                         data['prop_aligned'] = data['num_examples_aligned'] / data['num_examples_loaded']
@@ -29,6 +49,11 @@ def main():
                         t.add_row([data.get(field, '') for field in fields])
                     except (KeyError, AttributeError):
                         print('Skipping file:', fname)
+            latex += f'{model_to_name[model_version]} & {model_to_size[model_version]} & {bls_effect: .3f} &' \
+                     f'{bergsma_effect: .3f} \\\\\n\\hline\n'
+        with open(f'results/attention_intervention/winogender_{filter}_latex.txt', 'w') as o:
+            o.write(latex)
+
     t.align = 'r'
     t.float_format = '.3'
     with open('results/attention_intervention/winogender_summary.txt', 'w') as o:
