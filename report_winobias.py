93ab23add0574e3a5673ff69bfc16328941e0975
jessevig <jesse.vig@gmail.com>
2019-11-20 13:15:18 -0800

    Added remaining results for XL models and result for random model.

--- report_winobias.py
+++ report_winobias.py
@@ -9,30 +9,33 @@ def main():
               'mean_model_direct_effect', 'mean_sum_indirect_effect', 'prop_aligned', 'num_examples_loaded',
               'num_examples_analyzed']
     t = PrettyTable(fields)
-    for filter in ['filtered', 'unfiltered']:
-        for model_version in ['distilgpt2', 'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl']:
-            for split in ['dev', 'test']:
-                fname =  f"winobias_data/attention_intervention_{model_version}_{filter}_{split}.json"
-                if not os.path.exists(fname):
-                    print("File not available:", fname)
-                    continue
-                with open(fname) as f:
-                    data = json.load(f)
-                    # Populate 'prop_aligned'
-                    try:
-                        data['prop_aligned'] = data['num_examples_aligned'] / data['num_examples_loaded']
-                    except KeyError:
-                        data['prop_aligned'] = ''
-                    # Populate mean_sum_indirect
-                    results = data['results']
-                    df = pd.DataFrame(results)
-                    # Convert to shape (num_examples X num_layers X num_heads)
-                    indirect_by_head = np.stack(df['indirect_effect_head'].to_numpy())
-                    data['mean_sum_indirect_effect'] = indirect_by_head.sum(axis=(1, 2)).mean()
-                    try:
-                        t.add_row([data.get(field, '') for field in fields])
-                    except (KeyError, AttributeError):
-                        print('Skipping file:', fname)
+    for random_weights in False, True:
+        for filter in ['filtered', 'unfiltered']:
+            for model_version in ['distilgpt2', 'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl']:
+                for split in ['dev', 'test']:
+                    if random_weights:
+                        model_version += '_random'
+                    fname =  f"winobias_data/attention_intervention_{model_version}_{filter}_{split}.json"
+                    if not os.path.exists(fname):
+                        print("File not available:", fname)
+                        continue
+                    with open(fname) as f:
+                        data = json.load(f)
+                        # Populate 'prop_aligned'
+                        try:
+                            data['prop_aligned'] = data['num_examples_aligned'] / data['num_examples_loaded']
+                        except KeyError:
+                            data['prop_aligned'] = ''
+                        # Populate mean_sum_indirect
+                        results = data['results']
+                        df = pd.DataFrame(results)
+                        # Convert to shape (num_examples X num_layers X num_heads)
+                        indirect_by_head = np.stack(df['indirect_effect_head'].to_numpy())
+                        data['mean_sum_indirect_effect'] = indirect_by_head.sum(axis=(1, 2)).mean()
+                        try:
+                            t.add_row([data.get(field, '') for field in fields])
+                        except (KeyError, AttributeError):
+                            print('Skipping file:', fname)
 
     t.align = 'r'
     t.float_format = '.3'
