d22ee35b84be495d7a3944c1e8dbdd4537a5f278
jessevig <jesse.vig@gmail.com>
2019-11-22 12:04:09 -0800

    More vis updates.

--- attn_figures_example.py
+++ attn_figures_example.py
@@ -1,9 +1,10 @@
 import json
-
 import seaborn as sns
 import torch
 from matplotlib import pyplot as plt
 from transformers import GPT2Model, GPT2Tokenizer
+from operator import itemgetter
+from winobias import OCCUPATION_FEMALE_PCT
 
 def save_fig(prompts, heads, model, tokenizer, fname, device):
     attentions = []
@@ -34,7 +35,7 @@ def save_fig(prompts, heads, model, tokenizer, fname, device):
         head_names = []
 
         ax = axs[g_index]
-        ax.set_xlim([0, 0.6])
+        ax.set_xlim([0, 0.8])
         plts = []
         for i, (layer, head) in enumerate(heads):
             attn_last_word = attention[layer][head][-1].numpy()
@@ -84,24 +85,33 @@ def main():
     filter = 'filtered'
     split = 'dev'
     fname = f"winobias_data/attention_intervention_{model_version}_{filter}_{split}.json"
+
     with open(fname) as f:
         data = json.load(f)
     prompts = None
-    result_index = None
-    for i, result in enumerate(data['results']):
-        if result['base_string1'] == prompt + ' she':
-            prompts = (result['base_string1'], result['base_string2'])
-            result_index = i
-            break
-    assert prompts is not None
+    results = data['results']
+    # results_by_te = sorted(results, key=itemgetter('total_effect'), reverse=True)
+
+    def get_female_ratio(prompt):
+        female_pcts = []
+        for occupation, female_pct in OCCUPATION_FEMALE_PCT.items():
+            if occupation in prompt.lower():
+                female_pcts.append(female_pct)
+        assert len(female_pcts) == 2
+        return max(female_pcts) / min(female_pcts)
+
+    # results_by_ratio = sorted(results, key=lambda result: get_female_ratio(result['base_string1']), reverse=True)
+    results_by_ratio = sorted(results, key=itemgetter('total_effect'), reverse=True)
 
     with torch.no_grad():
         # Get attention and validate
         model = GPT2Model.from_pretrained(model_version, output_attentions=True)
         tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
         model.eval()
-        fname = f'results/attention_intervention/qualitative/winobias_{model_version}_{filter}_{split}_{result_index}.pdf'
-        save_fig(prompts, heads, model, tokenizer, fname, device)
+        for result_index, result in enumerate(results_by_ratio):
+            prompts = (result['base_string1'], result['base_string2'])
+            fname = f'results/attention_intervention/qualitative/winobias_{model_version}_{filter}_{split}_{result_index}.pdf'
+            save_fig(prompts, heads, model, tokenizer, fname, device)
 
 
 if __name__ == '__main__':
