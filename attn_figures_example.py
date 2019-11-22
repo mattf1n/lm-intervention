f9cd52910280acf67bbf157d8cc3b4d29e62ca17
jessevig <jesse.vig@gmail.com>
2019-11-22 06:33:28 -0800

    Updates to attention example vis.

--- attn_figures_example.py
+++ attn_figures_example.py
@@ -1,16 +1,10 @@
 import json
-from matplotlib import pyplot as plt
+
 import seaborn as sns
-import pandas as pd
-import numpy as np
-from attention_utils import topk_indices
-from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
-from mpl_toolkits.axes_grid1.colorbar import colorbar
-import os
-from experiment import Model
-from transformers import GPT2Model, GPT2Tokenizer
 import torch
-import matplotlib.ticker as mticker
+from matplotlib import pyplot as plt
+from transformers import GPT2Model, GPT2Tokenizer
+
 
 def main():
     sns.set_context("paper")
@@ -47,12 +41,11 @@ def main():
 
         fig, axs = plt.subplots(1, 2, sharey=True, figsize=(4,4))
         axs[0].invert_yaxis()
-        axs[0].tick_params(axis='y', which='major', pad=40)
+        axs[0].tick_params(axis='y', which='major', pad=42)
         axs[0].yaxis.set_label_position("right")
         axs[0].yaxis.tick_right()
         plt.setp(axs[0].get_yticklabels(), fontsize=14, ha='center')
         axs[0].yaxis.set_ticks_position('none')
-        # axs[0].xaxis.set_major_formatter(mticker.FuncFormatter(update_ticks))
 
         for g_index in range(2):
             prompt = prompts[g_index]
@@ -69,17 +62,13 @@ def main():
             attentions.append(attention)
             seq = tokenizer.convert_ids_to_tokens(input_[:-1]) + ["she/he"]
             seq = [t.replace('Ġ', '') for t in seq]
-            # sns.set_color_codes("pastel")
             heads = [(5,10), (5, 8)]
-            head_names = [f"Head {layer}-{head}" for layer, head in heads]
-            # f, ax = plt.subplots(figsize=(5, 5))
             left = 0
             prev = None
             head_names = []
 
             ax = axs[g_index]
             ax.set_xlim([0, 0.6])
-            # ax.invert_yaxis()
             plts = []
             for i, (layer, head) in enumerate(heads):
                 attn_last_word = attention[layer][head][-1].numpy()
@@ -99,27 +88,19 @@ def main():
                 prev = attn_last_word
             if g_index == 0:
                 ax.invert_xaxis()
-            # if g_index==1:
-            #     ax.tick_params(axis='y', which='major', pad=30)
-            #     ax.legend(plts, head_names, loc='lower right', fontsize=14)
-            #     plt.setp(ax.get_yticklabels(), fontsize=14, ha='center')
-            # else:
-            #     ax.yticks([])
             plt.setp(ax.get_xticklabels(), fontsize=14)
             sns.despine(left=True, bottom=True)
             if g_index==1:
-                leg  = ax.legend(plts, head_names, fontsize=12, handlelength=.9, handletextpad=.4, bbox_to_anchor = [0.1, 0.17])#, color=['#4C71B0', '#DD8452'])
-                # leg = ax.get_legend()
+                leg  = ax.legend(plts, head_names, fontsize=12, handlelength=.9, handletextpad=.4, bbox_to_anchor = [0.1, 0.17])
                 leg.legendHandles[0].set_color('#4C71B0')
                 leg.legendHandles[1].set_color('#DD8452')
 
         axs[0].get_yticklabels()[0].set_color('#AFAFAF')
         fname = f'results/attention_intervention/qualitative/winobias_{model_version}_{filter}_{split}_{result_index}.pdf'
-        # plt.show()
-        # plt.savefig(fname, format='png', dpi=fig.dpi)
+
+        plt.tight_layout()
         plt.savefig(fname, format='pdf')
         plt.close()
-        # plt.show()
 
 
 if __name__ == '__main__':
