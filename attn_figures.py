f319cb55efcb47cdf8d2675562a85d6ee118ac32
jessevig <jesse.vig@gmail.com>
2019-12-10 16:24:35 -0800

    Assorted figure changes.

--- attn_figures.py
+++ attn_figures.py
@@ -7,6 +7,7 @@ from attention_utils import topk_indices
 from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 from mpl_toolkits.axes_grid1.colorbar import colorbar
 from  matplotlib.colors import LinearSegmentedColormap
+import matplotlib.ticker as ticker
 import os
 
 def save_figures(data, source, model_version, filter, suffix, k=10):
@@ -37,7 +38,7 @@ def save_figures(data, source, model_version, filter, suffix, k=10):
 
     # Plot stacked bar chart
     palette = sns.color_palette()#('muted')
-    plt.figure(num=1, figsize=(5, 3))
+    plt.figure(num=1, figsize=(5, 2))
     topk_direct = []
     topk_indirect = []
     labels = []
@@ -56,7 +57,12 @@ def save_figures(data, source, model_version, filter, suffix, k=10):
     plt.yticks(size=10)
     # plt.yticks(np.arange(0, 81, 10))
     p3 = plt.axhline(data['mean_total_effect'], linestyle='--')
-    plt.legend((p3, p2[0], p1[0]), ('Total', 'Direct', 'Indirect'), loc='lower right', fontsize=11)
+
+    # lgd = plt.figlegend(plts, head_names, 'lower center', fontsize=7, borderpad=0.5, handlelength=.9,
+    #                     handletextpad=.3, labelspacing=0.15, bbox_to_anchor=(0.86, 0.11))
+
+    plt.legend((p3, p2[0], p1[0]), ('Total', 'Direct', 'Indirect'), loc='upper right', fontsize=11, bbox_to_anchor=(.99, 0.90))
+    sns.despine()
     plt.savefig(f'results/attention_intervention/stacked_bar_charts/{source}_{model_version}_{filter}_'
                 f'{suffix}.pdf', format='pdf')
     plt.close()
@@ -102,22 +108,23 @@ def save_figures(data, source, model_version, filter, suffix, k=10):
                     continue
                 effect_head = mean_direct_by_head
                 effect_layer = mean_direct_by_layer
-            fig = plt.figure(figsize=(4.3, 3.8))
+            # fig = plt.figure(figsize=(3.1, 1.93))
+            fig = plt.figure(figsize=(3, 2.2))
             if model_version == 'distilgpt2':
-                ax1 = plt.subplot2grid((100, 85), (0, 2), colspan=60, rowspan=99)
-                ax2 = plt.subplot2grid((100, 85), (17, 67), colspan=17, rowspan=41)
+                ax1 = plt.subplot2grid((100, 85), (0, 0), colspan=62, rowspan=99)
+                ax2 = plt.subplot2grid((100, 85), (32, 69), colspan=17, rowspan=35)
             elif model_version in ('gpt2', 'gpt2_random'):
-                ax1 = plt.subplot2grid((100, 85), (0, 0), colspan=60, rowspan=99)
-                ax2 = plt.subplot2grid((100, 85), (0, 62), colspan=15, rowspan=75)
+                ax1 = plt.subplot2grid((100, 85), (0, 0), colspan=65, rowspan=99)
+                ax2 = plt.subplot2grid((100, 85), (12, 70), colspan=15, rowspan=75)
             elif model_version == 'gpt2-medium':
-                ax1 = plt.subplot2grid((100, 85), (0, 12), colspan=40, rowspan=75)
-                ax2 = plt.subplot2grid((100, 85), (0, 54), colspan=17, rowspan=75)
+                ax1 = plt.subplot2grid((100, 85), (0, 5), colspan=55, rowspan=99)
+                ax2 = plt.subplot2grid((100, 85), (2, 64), colspan=17, rowspan=95)
             elif model_version == 'gpt2-large':
-                ax1 = plt.subplot2grid((100, 85), (0, 16), colspan=32, rowspan=75)
-                ax2 = plt.subplot2grid((100, 85), (0, 51), colspan=17, rowspan=75)
+                ax1 = plt.subplot2grid((100, 85), (0, 5), colspan=55, rowspan=96)
+                ax2 = plt.subplot2grid((100, 85), (0, 62), colspan=17, rowspan=97)
             elif model_version == 'gpt2-xl':
-                ax1 = plt.subplot2grid((100, 85), (0, 16), colspan=32, rowspan=75)
-                ax2 = plt.subplot2grid((100, 85), (0, 51), colspan=17, rowspan=75)
+                ax1 = plt.subplot2grid((100, 85), (0, 5), colspan=55, rowspan=96)
+                ax2 = plt.subplot2grid((100, 85), (0, 62), colspan=17, rowspan=97)
             # heatmap = sns.heatmap(effect_head, vmin=-0.05, vmax=0.05, rasterized=True, ax=ax1, annot=annot, annot_kws={"size": 9}, fmt=".2f", square=True, cbar=False,cmap=sns.color_palette("coolwarm", 256))
             # heatmap = sns.heatmap(effect_head, vmin=-0.05, vmax=0.05, ax=ax1, annot=annot, annot_kws={"size": 9}, fmt=".2f", square=True, cbar=False,cmap=sns.color_palette("coolwarm_r", 256), linewidth=0.3)
             # heatmap = sns.heatmap(effect_head, vmin=-0.05, vmax=0.05, ax=ax1, annot=annot, annot_kws={"size": 9}, fmt=".2f", square=True, cbar=False, linewidth=0.1, linecolor='#DDDDDD',
@@ -126,6 +133,20 @@ def save_figures(data, source, model_version, filter, suffix, k=10):
             heatmap = sns.heatmap(effect_head, center=0.0, ax=ax1, annot=annot, annot_kws={"size": 9}, fmt=".2f", square=True, cbar=False, linewidth=0.1, linecolor='#D0D0D0',
             # cmap = LinearSegmentedColormap.from_list('rg', ["r", "#DDDCDB", "#40bcae"], N=256))
             cmap = LinearSegmentedColormap.from_list('rg', ["#F14100", "white", "#3D4FC4"], N=256))
+            plt.setp(heatmap.get_yticklabels(), fontsize=7)
+            plt.setp(heatmap.get_xticklabels(), fontsize=7)
+            heatmap.tick_params(axis='x', pad=1, length=2)
+            heatmap.tick_params(axis='y', pad=1, length=2)
+            heatmap.yaxis.labelpad = 2
+            heatmap.invert_yaxis()
+            if model_version != 'gpt2-xl':
+                for i, label in enumerate(heatmap.xaxis.get_ticklabels()):
+                    if i%2 == 1:
+                        label.set_visible(False)
+                for i, label in enumerate(heatmap.yaxis.get_ticklabels()):
+                    if i%2 == 1:
+                        label.set_visible(False)
+
 
             # https://stackoverflow.com/questions/38246559/how-to-create-a-heat-map-in-python-that-ranges-from-green-to-red
             if do_sort:
@@ -142,30 +163,43 @@ def save_figures(data, source, model_version, filter, suffix, k=10):
             # split axes of heatmap to put colorbar
             ax_divider = make_axes_locatable(ax1)
             # # define size and padding of axes for colorbar
-            if model_version == 'distilgpt2':
-                cax = ax_divider.append_axes('bottom', size='10%', pad='50%')
-            elif model_version in ('gpt2-xl', 'gpt2-large', 'gpt2-medium'):
-                cax = plt.subplot2grid((100, 85), (95, 10), colspan=45, rowspan=4)
+            # if model_version == 'distilgpt2':
+            #     cax = ax_divider.append_axes('bottom', size='10%', pad='50%')
+            # elif model_version in ('gpt2-xl', 'gpt2-large', 'gpt2-medium'):
+            #     cax = plt.subplot2grid((100, 85), (95, 10), colspan=45, rowspan=4)
+            # else:
+            if model_version in ('gpt2-large', 'gpt2-xl'):
+                cax = ax_divider.append_axes('left', size='7%', pad='45%')
             else:
-                cax = ax_divider.append_axes('bottom', size='7%', pad='25%')
+                cax = ax_divider.append_axes('left', size='7%', pad='33%')
             # # make colorbar for heatmap.
             # # Heatmap returns an axes obj but you need to get a mappable obj (get_children)
-            cbar = colorbar(ax1.get_children()[0], cax=cax, orientation='horizontal')
+            cbar = colorbar(ax1.get_children()[0], cax=cax, orientation='vertical')
+            cax.yaxis.set_ticks_position('left')
             cbar.solids.set_edgecolor("face")
+            cbar.ax.tick_params(labelsize=7, length=4, pad=2)
+
             # # locate colorbar ticks
-            cax.xaxis.set_ticks_position('bottom')
-            ax1.set(xlabel='Head', ylabel='Layer', title='Head Effect')
+            # ax1.set(xlabel='Head', ylabel='Layer', title='Head Effect')
+            # ax1.set(xlabel='Head', ylabel='Layer', title='Head Effect')
+            ax1.set_title('Head Effect', size=9)
+            ax1.set_xlabel('Head', size=8)
+            ax1.set_ylabel('Layer', size=8)
+
             for _, spine in ax1.spines.items():
                 spine.set_visible(True)
-            ax2.set(title=f'     Layer Effect')
+            ax2.set_title('         Layer Effect', size=9)
             # sns.set_style("ticks")
             # sns.barplot(x=effect_layer, ax=ax2, y=list(range(n_layers)), color="#4472C4", orient="h")
             # sns.barplot(x=effect_layer, ax=ax2, y=list(range(n_layers)), color="#3A4BC0", orient="h")
             # sns.barplot(x=effect_layer, ax=ax2, y=list(range(n_layers)), color="#595959", orient="h")
             # sns.barplot(x=effect_layer, ax=ax2, y=list(range(n_layers)), color="#40bcae", orient="h")
-            sns.barplot(x=effect_layer, ax=ax2, y=list(range(n_layers)), color="#3D4FC4", orient="h")
+            bp = sns.barplot(x=effect_layer, ax=ax2, y=list(range(n_layers)), color="#3D4FC4", orient="h")
+            plt.setp(bp.get_xticklabels(), fontsize=7)
+            bp.tick_params(axis='x', pad=1, length=3)
             # sns.barplot(x=effect_layer, ax=ax2, y=list(range(n_layers)), color="g", orient="h")
             # ax2.set_frame_on(False)
+            ax2.invert_yaxis()
             ax2.set_yticklabels([])
             ax2.spines['top'].set_visible(False)
             ax2.spines['right'].set_visible(False)
@@ -186,6 +220,10 @@ def main():
 
     model_versions = ['distilgpt2', 'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl', 'gpt2_random']
     filters = ['filtered', 'unfiltered']
+
+    # model_versions = ['gpt2']
+    # filters = ['filtered']
+
 
     # For testing:
     #
