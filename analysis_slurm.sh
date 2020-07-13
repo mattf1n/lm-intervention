fa25f14c931b492b07f87e1f85e15dd52140faf8
Matthew Finlayson <mattf1n@boslogin03.rc.fas.harvard.edu>
2020-07-13 17:19:07 -0400

    seaborn figures

--- analysis_slurm.sh
+++ analysis_slurm.sh
@@ -3,11 +3,9 @@
 #SBATCH -N 1                # Ensure that all cores are on one machine
 #SBATCH -t 0-10:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p seas_dgx1        # Partition to submit to
-#SBATCH --mem=100000         # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=200000         # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 module load Anaconda3/2019.10
 source activate myenv
-pip install pandas
-pip install pyarrow
 python analysis.py results/na_neuron_intervention/ figures/
