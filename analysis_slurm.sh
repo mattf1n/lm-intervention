b74e1b96ea04b72ae927f6affbeed37989e0cee2
Matthew Finlayson <mattf1n@boslogin01.rc.fas.harvard.edu>
2020-07-16 12:02:34 -0400

    figs

--- analysis_slurm.sh
+++ analysis_slurm.sh
@@ -8,4 +8,4 @@
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 module load Anaconda3/2019.10
 source activate myenv
-python analysis.py results/na_neuron_intervention/ figures/
+python analysis.py results/na_neuron_intervention/ figures/ false
