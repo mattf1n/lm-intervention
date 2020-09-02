aa30af20cee8c1574e02b4f5d077ea06ddc243bb
Matthew Finlayson <>
2020-09-01 21:11:58 -0700

    fixed aggregation of calculated values

--- make_feathers.py
+++ make_feathers.py
@@ -23,8 +23,6 @@ def get_example_type(f):
 
 def compute_effects_and_save():
     files = glob(PATH + '*.csv')
-    print(PATH)
-    print(files)
     preloaded = glob(PATH + '*.feather')
     effects_dfs = []
     agg_dfs = []
@@ -39,31 +37,40 @@ def compute_effects_and_save():
             df.to_feather(feather)
         df['Layer'] = df.layer
         df['Neuron'] = df.neuron
+        df = df.set_index(['Neuron', 'Layer'])
         df['Random'] = 'random' in f
         df['Model size'] = get_size(f)
         df['Intervening tokens'] = get_example_type(f)
         df['Effect type'] = 'Indirect' if 'indirect' in f else 'Direct'
+
+        # Aggregated calculated values
+        agg = df.groupby(['Neuron', 'Layer']).mean()
+        agg['Yz'] = agg.candidate2_prob / agg.candidate1_prob
+        agg['Singular grammaticality'] = agg.candidate2_base_prob \
+                / agg.candidate1_base_prob
+        agg['Plural grammaticality'] = agg.candidate1_alt1_prob \
+                / agg.candidate2_alt1_prob
+        agg['Effect'] = agg['Yz'] / agg['Singular grammaticality'] - 1
+        agg['Total effect'] = 1 / (agg['Plural grammaticality'] 
+                * agg['Singular grammaticality']) - 1 
+        neurons_per_layer, _ = agg.index.max()
+        idx = agg.sort_values('Effect').groupby('Layer')\
+                .tail(int(neurons_per_layer*0.05)).index
+        agg['Top 5 percent'] = agg.index.isin(idx)
+        agg_dfs.append(agg)
+
+        # Unaggregated calculated values
         df['Yz'] = df.candidate2_prob / df.candidate1_prob
         df['Singular grammaticality'] = df.candidate2_base_prob \
                 / df.candidate1_base_prob
         df['Effect'] = df['Yz'] / df['Singular grammaticality'] - 1
         df['Plural grammaticality'] = df.candidate1_alt1_prob \
                 / df.candidate2_alt1_prob
-        df['Total effect'] = 1 \
-                / (df['Plural grammaticality'] 
-                        * df['Singular grammaticality']) \
-                - 1
-        neurons = ['Neuron', 'Layer']
-        df = df.set_index(neurons)
-        neurons_per_layer = len(df.groupby('Neuron').mean().index)
-        idx = df.groupby(neurons).mean().sort_values('Effect')\
-                .groupby('Layer')\
-                .tail(int(neurons_per_layer*0.05)).index
-        df['Top 5 percent'] = df.index.isin(idx)
+        df['Total effect'] = 1 / (df['Plural grammaticality'] 
+                * df['Singular grammaticality']) - 1
         effects_dfs.append(df)
-        agg_dfs.append(df.groupby(neurons).mean())
-    pd.concat(effects_dfs).reset_index()\
-            .to_feather(PATH + 'effects.feather')
+
+    pd.concat(effects_dfs).reset_index().to_feather(PATH + 'effects.feather')
     pd.concat(agg_dfs).reset_index().to_feather(PATH + 'agg.feather')
     
 if __name__ == "__main__":
