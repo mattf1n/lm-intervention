964c9135d2305a0d9212465999a6ba187468517f
Matthew Finlayson <>
2020-10-30 19:33:12 -0600

    added runscript generator

--- winobias_filtered_test.sh
+++
@@ -1,4 +0,0 @@
-python winobias_attn_intervention.py --gpt2-version distilgpt2 --do-filter True --split test &&
-python winobias_attn_intervention.py --gpt2-version gpt2 --do-filter True --split test &&
-python winobias_attn_intervention.py --gpt2-version gpt2-medium --do-filter True --split test &&
-python winobias_attn_intervention.py --gpt2-version gpt2-large --do-filter True --split test
--- winobias_xl.sh
+++
@@ -1,4 +0,0 @@
-python winobias_attn_intervention.py --gpt2-version gpt2-xl --do-filter True --split dev &&
-python winobias_attn_intervention.py --gpt2-version gpt2-xl --do-filter True --split test &&
-python winobias_attn_intervention.py --gpt2-version gpt2-xl --do-filter False --split dev &&
-python winobias_attn_intervention.py --gpt2-version gpt2-xl --do-filter False --split test
